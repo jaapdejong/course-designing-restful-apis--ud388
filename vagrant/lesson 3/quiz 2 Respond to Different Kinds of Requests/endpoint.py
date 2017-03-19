9ac27034f5a7aa340c6885596f74a0210b18b86a
Jaap de Jong <jaap.dejong@nedap.com>
2017-03-19 20:55:01 +0100

    Update lesson 3 quiz 2

--- vagrant/lesson 3/quiz 2 Respond to Different Kinds of Requests/endpoint.py
+++ vagrant/lesson 3/quiz 2 Respond to Different Kinds of Requests/endpoint.py
@@ -7,12 +7,10 @@ app = Flask(__name__)
 def puppiesFunction():
 	if request.method == 'GET':
 		#Call the method to Get all of the puppies
-		print 'GET /puppies'
 		return getAllPuppies()
 
 	elif request.method == 'POST':
 		#Call the method to make a new puppy
-		print 'POST /puppies'
 		return makeANewPuppy()  
   
  
@@ -21,38 +19,30 @@ def puppiesFunction():
 def puppiesFunctionId(id):
 	if request.method == 'GET':
 		#Call the method to get a specific puppy based on their id
-		print 'GET /puppies/' + str(id)
 		return getPuppy(id)
 		
 	elif request.method == 'PUT':
 		#Call the method to update a puppy
-		print 'PUT /puppies/' + str(id)
 		return updatePuppy(id)
 		
 	elif request.method == 'DELETE':
 		#Call the method to remove a puppy
-		print 'DELETE /puppies/' + str(id)
 		return deletePuppy(id)
 
 
 def getAllPuppies():
-	print 'getAllPuppies()'
 	return "Getting All the puppies!"
 
 def makeANewPuppy():
-	print 'makeANewPuppy()'
 	return "Creating A New Puppy!"
 
 def getPuppy(id):
-	print 'getPuppy(' + str(id) + ')'
 	return "Getting Puppy with id %s" % id
 
 def updatePuppy(id):
-	print 'updatePuppy(' + str(id) + ')'
 	return "Updating a Puppy with id %s" % id
 
 def deletePuppy(id):
-	print 'deletePuppy(' + str(id) + ')'
 	return "Removing Puppy with id %s" % id
 
 if __name__ == '__main__':
